98ade3bb9add43cdd0e7ac0824f66e57648c866e
bedar89 <bedar89@gmail.com>
2022-06-07 15:44:16 +0000

    added slots for qb-slots

--- init.lua
+++ init.lua
@@ -74,6 +74,36 @@ Config.TargetBones = {
 }
 
 Config.TargetModels = {
+	["qb-slots"] = {
+		models = {
+			`ch_prop_casino_slot_01a`,
+			`ch_prop_casino_slot_02a`,
+			`ch_prop_casino_slot_03a`,
+			`ch_prop_casino_slot_04a`,
+			`ch_prop_casino_slot_04b`,
+			`ch_prop_casino_slot_05a`,
+			`ch_prop_casino_slot_06a`,
+			`ch_prop_casino_slot_07a`,
+			`ch_prop_casino_slot_08a`,
+			`vw_prop_casino_slot_01a`,
+			`vw_prop_casino_slot_02a`,
+			`vw_prop_casino_slot_03a`,
+			`vw_prop_casino_slot_04a`,
+			`vw_prop_casino_slot_05a`,
+			`vw_prop_casino_slot_06a`,
+			`vw_prop_casino_slot_07a`,
+			`vw_prop_casino_slot_08a`,
+		},
+		options = {
+			{
+				type = "client",
+				event = "qb-slots:enterBets",
+				icon = "fa-solid fa-slot-machine",
+				label = "Play Slot Machine",
+			},
+		},
+		distance = 2.5,
+	},
 
 }
 
